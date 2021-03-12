2d5852cdad40388ceb7abc094e9c67fd8a6307e9
Mr-Yellow <mr-yellow@mr-yellow.com>
2021-03-12 12:12:57 +1100

    CHORE: CommonJS tests to work with non-module package

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-export {default as DefaultParsers} from './lib/DefaultParsers';
-export {default as DocReader} from './lib/DocReader';
-export {default as DocResponse} from './lib/DocResponse';
-
-/*
-module.exports = {
-	DefaultParsers: require('./lib/DefaultParsers'),
-	DocReader: require('./lib/DocReader'),
-	DocResponse: require('./lib/DocResponse'),
-};
-*/
+export {default as DefaultParsers} from './lib/DefaultParsers.js';
+export {default as DocReader} from './lib/DocReader.js';
+export {default as DocResponse} from './lib/DocResponse.js';
